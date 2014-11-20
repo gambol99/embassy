93c6457e3296939a1e6d655654c2847c03bd2e6d
Rohith <gambol99@gmail.com>
2014-11-20 15:23:59 +0000

    - fixing up the adding and removing of endpoints

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -19,6 +19,7 @@ package discovery
 import (
 	"errors"
 	"regexp"
+	"sync"
 
 	"github.com/gambol99/embassy/config"
 	"github.com/gambol99/embassy/services"
@@ -29,21 +30,20 @@ const (
 	DISCOVERY_STORES = `^(consul|etcd):\/\/`
 )
 
-type DiscoveryEvent struct {
-	Endpoint services.Endpoint
-}
-
-type DiscoveryStoreChannel chan DiscoveryEvent
+type DiscoveryStoreChannel chan services.Service
 
 type DiscoveryStore interface {
 	ListEndpoints() ([]services.Endpoint, error)
 	WatchEndpoints(channel DiscoveryStoreChannel)
+	Synchronize(*services.Service) error
 }
 
 type DiscoveryStoreService struct {
-	Service services.Service
-	Store   DiscoveryStoreProvider
-	Config  *config.Configuration
+	sync.RWMutex
+	Service      services.Service
+	Store        DiscoveryStoreProvider
+	Config       *config.Configuration
+	EndpointsMap map[services.EndpointID]services.Endpoint
 }
 
 type DiscoveryStoreProvider interface {
@@ -51,8 +51,8 @@ type DiscoveryStoreProvider interface {
 	Watch(*services.Service)
 }
 
-func NewDiscoveryService(config *config.Configuration, service services.Service) (DiscoveryStore, error) {
-	glog.Infof("Creating a new discovery agent for service: %s", service)
+func NewDiscoveryService(config *config.Configuration, si services.Service) (DiscoveryStore, error) {
+	glog.Infof("Creating a new discovery agent for service: %s", si)
 	/* step: check if the store provider is supported */
 	if !IsDiscoveryStore(config.DiscoveryURI) {
 		return nil, errors.New("The backend discovery store specified is not supported")
@@ -60,7 +60,11 @@ func NewDiscoveryService(config *config.Configuration, service services.Service)
 	var provider DiscoveryStoreProvider
 	var err error
 	discovery := new(DiscoveryStoreService)
+	discovery.Service = si
 	discovery.Config = config
+	discovery.EndpointsMap = make(map[services.EndpointID]services.Endpoint, 0)
+
+	/* step: create the backend provioder */
 	switch config.DiscoveryURI {
 	case "etcd":
 		glog.Infof("Using Etcd as discovery backend, uri: %s", config.DiscoveryURI)
@@ -77,34 +81,55 @@ func NewDiscoveryService(config *config.Configuration, service services.Service)
 	return discovery, nil
 }
 
-func (ds DiscoveryStoreService) ListEndpoints() ([]services.Endpoint, error) {
+func (ds *DiscoveryStoreService) ListEndpoints() (endpoints []services.Endpoint, err error) {
 	/* step: pull a list of paths from the backend */
-	endpoints, err := ds.Store.List(&ds.Service)
-	if err != nil {
-		glog.Errorf("Unable to retrieve endpoints from the store, error: %s", err)
-		return nil, err
+	ds.RLock()
+	defer ds.RUnlock()
+	endpoints = make([]services.Endpoint, len(ds.EndpointsMap))
+	for _, endpoint := range ds.EndpointsMap {
+		endpoints = append(endpoints, endpoint)
 	}
-	if len(endpoints) <= 0 {
-		glog.V(2).Infof("We have no endpoints avaible for service: %s", ds.Service)
-	}
-	/* step: iterate and pull build the endpoints */
-	return endpoints, nil
+	return
 }
 
-func (d DiscoveryStoreService) WatchEndpoints(channel DiscoveryStoreChannel) {
-	glog.V(2).Info("Watching service: %s", d.Service)
-	go func() {
-		for {
-			/* step: block and wait for something, anything to change */
-			d.Store.Watch(&d.Service)
-			/* step: pull an updated list of the endpoints
-			if endpoints, err := d.ListEndpoints(); err != nil {
-				glog.Error("Unable to retrieve an updated list of paths from factory, error:", err)
-				continue
-			}
-			*/
+func (ds *DiscoveryStoreService) Synchronize(service *services.Service) error {
+	glog.V(3).Infof("Resynchronizing the endpoints for service: %s", service)
+	ds.Lock()
+	defer ds.Unlock()
+	endpoints, err := ds.Store.List(service)
+	if err != nil {
+		glog.Errorf("Attempt to resynchronize the endpoints failed for service: %s, error: %s", service, err)
+		return errors.New("Failed to resync the endpoints")
+	}
+	/* step: we register any new endpoints - using the endpoint id as key into the map */
+	registeredEndpoints := make(map[services.EndpointID]bool)
+	for _, endpoint := range endpoints {
+		registeredEndpoints[endpoint.ID] = true
+		if _, found := ds.EndpointsMap[endpoint.ID]; !found {
+			ds.EndpointsMap[endpoint.ID] = endpoint
 		}
-	}()
+	}
+	/* step: we remove anything that should no longer be there */
+	for id, _ := range ds.EndpointsMap {
+		if _, exists := registeredEndpoints[id]; !exists {
+			delete(ds.EndpointsMap, id)
+		}
+	}
+	return nil
+}
+
+func (ds *DiscoveryStoreService) WatchEndpoints(channel DiscoveryStoreChannel) {
+	glog.V(2).Info("Watching service: %s", ds.Service)
+	go func(ds *DiscoveryStoreService) {
+		for {
+			glog.V(4).Infof("Waiting for endpoints on service: %s to change", ds.Service)
+			/* step: block and wait for something, anything to change */
+			ds.Store.Watch(&ds.Service)
+			glog.V(4).Infof("Endpoints has changed for service: %s", ds.Service)
+			/* step: pull an updated list of the endpoints */
+			channel <- ds.Service
+		}
+	}(ds)
 }
 
 func IsDiscoveryStore(uri string) bool {
